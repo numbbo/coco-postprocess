b45df208972793137abf17f3377ef52444769390
Joshua, Alexander <joshua.alexander@gmail.com>
2025-11-16 18:34:42 +0100

    Another attempt

--- src/cocopp/html/index.py
+++ src/cocopp/html/index.py
@@ -1,6 +1,5 @@
 """Main interface for generating folder index pages."""
 
-from typing import Dict
 import logging
 import os
 from .generator import HtmlGenerator, HtmlContent  
@@ -18,18 +17,21 @@ def collect_algorithm_data(directory):
     if not os.path.isdir(directory):
         return data
     
-    for item in os.listdir(directory):
-        item_path = os.path.join(directory, item)
-        if not os.path.isdir(item_path):
-            continue
-        
-        single_file = os.path.join(item_path, '%s.html' % genericsettings.single_algorithm_file_name)
-        if os.path.isfile(single_file):
-            data['single'].append(item)
-        
-        many_file = os.path.join(item_path, '%s.html' % genericsettings.many_algorithm_file_name)
-        if os.path.isfile(many_file):
-            data['comparison'].append(item)
+    try:
+        for item in os.listdir(directory):
+            item_path = os.path.join(directory, item)
+            if not os.path.isdir(item_path):
+                continue
+            
+            single_file = os.path.join(item_path, '%s.html' % genericsettings.single_algorithm_file_name)
+            if os.path.isfile(single_file):
+                data['single'].append(item)
+            
+            many_file = os.path.join(item_path, '%s.html' % genericsettings.many_algorithm_file_name)
+            if os.path.isfile(many_file):
+                data['comparison'].append(item)
+    except OSError as e:
+        logger.warning("Error reading directory %s: %s" % (directory, str(e)))
     
     return data
 
@@ -60,17 +62,23 @@ def update_parent_index(parent_index_path):
         if algo_data['comparison']:
             links.append('<H2>Comparison Data</H2>')
             for algo in sorted(algo_data['comparison']):
-                path = os.path.join(algo, '%s.html' % genericsettings.many_algorithm_file_name)
-                link = generator.add_link(path, algo, '&nbsp;&nbsp;')
-                links.append(link)
+                # Use absolute path for checking, relative for linking
+                abs_path = os.path.join(parent_dir, algo, '%s.html' % genericsettings.many_algorithm_file_name)
+                rel_path = os.path.join(algo, '%s.html' % genericsettings.many_algorithm_file_name)
+                if os.path.isfile(abs_path):
+                    link = generator.add_link(rel_path, algo, '&nbsp;&nbsp;')
+                    links.append(link)
                 
         # Add single algorithm section
         if algo_data['single']:
             links.append('<H2>Single Algorithm Data</H2>')
             for algo in sorted(algo_data['single']):
-                path = os.path.join(algo, '%s.html' % genericsettings.single_algorithm_file_name)
-                link = generator.add_link(path, algo, '&nbsp;&nbsp;')
-                links.append(link)
+                # Use absolute path for checking, relative for linking
+                abs_path = os.path.join(parent_dir, algo, '%s.html' % genericsettings.single_algorithm_file_name)
+                rel_path = os.path.join(algo, '%s.html' % genericsettings.single_algorithm_file_name)
+                if os.path.isfile(abs_path):
+                    link = generator.add_link(rel_path, algo, '&nbsp;&nbsp;')
+                    links.append(link)
                 
         # Create content object
         content = HtmlContent(
@@ -105,22 +113,27 @@ def save_folder_index(filepath, image_extension):
     """
     if not filepath:
         return
+    
+    try:
+        # Generate content
+        generator = HtmlGenerator()
+        current_dir = os.path.dirname(os.path.realpath(filepath))
+        content = generator.generate_folder_content(current_dir, image_extension)
         
-    # Generate content
-    generator = HtmlGenerator()
-    current_dir = os.path.dirname(os.path.realpath(filepath))
-    content = generator.generate_folder_content(current_dir, image_extension)
-    
-    # Render to HTML
-    html = generator.render(content)
-    
-    # Safely write to file with backup
-    writer = HtmlWriter()
-    writer.backup_if_exists(filepath)
-    writer.write_safely(filepath, html)
-    
-    # Update parent index if needed
-    parent_dir = os.path.dirname(current_dir)
-    parent_index_path = os.path.join(parent_dir, 'index.html')
-    if os.path.isfile(parent_index_path):
-        update_parent_index(parent_index_path)
+        # Render to HTML
+        html = generator.render(content)
+        
+        # Safely write to file with backup
+        writer = HtmlWriter()
+        writer.backup_if_exists(filepath)
+        writer.write_safely(filepath, html)
+        
+        # Update parent index if needed
+        parent_dir = os.path.dirname(current_dir)
+        parent_index_path = os.path.join(parent_dir, 'index.html')
+        if os.path.isfile(parent_index_path):
+            update_parent_index(parent_index_path)
+            
+    except Exception as e:
+        logger.error("Failed to save folder index at %s: %s" % (filepath, str(e)))
+        raise
